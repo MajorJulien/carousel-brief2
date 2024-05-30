2ffd86e1eda17e816f849c100a32157f8299998a
Julien Salomé <j.salome.pro@gmail.com>
2024-05-30 15:49:33 +0200

    feat(carousel.js): add script js

---
+++ carousel.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("mySlides");
+
+function showSlide(n) {
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = n;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+function next() {
+  showSlide(slideIndex + 1);
+}
+
+showSlide(slideIndex);
